329485ea45af95258b34ae3fca879f1422473126
Gaggun Chaggar <gaggunchaggar@gmail.com>
2022-09-30 18:25:44 +0100

    update react to prevent default onclick events for tabs

--- packages/gsc-ui/react/src/components/gsc-tabs/gsc-tabs.ts
+++ packages/gsc-ui/react/src/components/gsc-tabs/gsc-tabs.ts
@@ -8,6 +8,7 @@ import {
   Slots,
 } from '@gsc-ui/core/dist/components/gsc-tabs/gsc-tabs'
 import { MouseEvent, ReactNode, useState } from 'react'
+import { keys } from '@gsc/utils'
 
 type RProps = ReactProps &
   Props &
@@ -17,11 +18,18 @@ type RProps = ReactProps &
 export const GscTabs = (props: RProps) => {
   const s = useSettings()
 
-  const [activeTab, setActiveTab] = useState('')
+  const [activeTab, setActiveTab] = useState(keys(props.tabs)[0])
+
+  const events = {
+    onClick: (e: MouseEvent<HTMLButtonElement>, i: number) => {
+      e.preventDefault()
+      props.onClick(e, i)
+    },
+  }
 
   const r = coreGscTabs(s, 'className')(
     props,
-    props,
+    events,
     { activeTab },
     { activateTab: setActiveTab }
   )
